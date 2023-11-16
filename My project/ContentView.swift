2e6a703f1d9ad7d6b8029168245f31c1bc86fcd5
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-11-16 12:13:10 +0100

    Initial Commit

---
+++ My project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My project
+//
+//  Created by Mohamad Hleihel on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My project/My_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_projectApp.swift
+//  My project
+//
+//  Created by Mohamad Hleihel on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct My_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
